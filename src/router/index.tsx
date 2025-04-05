d87d760da4e32b4e71c3d6db25f498202c734f40
hbzzll <hbzz0823@163.com>
2025-04-05 12:57:11 +0200

    router

---
+++ src/router/index.tsx
@@ -0,0 +1,34 @@
+import { lazy } from "react";
+import Home from "../view/Home";
+// import About from "../view/About";
+
+const Page1 = lazy(() => import("../view/Page1"));
+const Page2 = lazy(() => import("../view/Page2"));
+const About = lazy(() => import("../view/About"));
+
+//navigate to redirect to home page
+import { Navigate } from "react-router-dom";
+import path from "path";
+
+const routes = [
+  {
+    path: "/",
+    element: <Navigate to="/page2" />, // redirect to home page
+  },
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/page1",
+        element: <Page1 />,
+      },
+      {
+        path: "/page2",
+        element: <Page2 />,
+      },
+    ],
+  },
+];
+
+export default routes;
