fa4d5dd1c4e31a86de693fcc5498b0d7f870adca
hbzzll <hbzz0823@163.com>
2025-04-10 10:28:27 +0200

    log in interface

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
 import { navlist } from "../../Data/data";
+import LoginModal from "../Login/login";
 
 const Header = () => {
   const [navList, setNavList] = useState(false);
+  const [open, setopen] = useState(false);
 
   return (
     <>
@@ -28,7 +30,7 @@ const Header = () => {
             <h4>
               <span>2</span> My List
             </h4>
-            <button className="btn1">
+            <button className="btn1" onClick={() => setopen(true)}>
               <i className="fa fa-sign-out"></i> Sign In
             </button>
           </div>
@@ -44,6 +46,7 @@ const Header = () => {
           </div>
         </div>
       </header>
+      <LoginModal open={open} onClose={() => setopen(false)} />
     </>
   );
 };
