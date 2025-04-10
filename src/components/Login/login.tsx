9df0a2c731c10825cdac02f466ebfd53600d0f6e
hbzzll <hbzz0823@163.com>
2025-04-10 21:47:08 +0200

    login interface-version2

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -1,72 +1,6 @@
-// import React, { useState } from "react";
-// import { ExclamationCircleOutlined } from "@ant-design/icons";
-// import { Button, Modal, Space } from "antd";
-
-// const LocalizedModal = () => {
-//   const [open, setOpen] = useState(false);
-
-//   const showModal = () => {
-//     setOpen(true);
-//   };
-
-//   const hideModal = () => {
-//     setOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <Button type="primary" onClick={showModal}>
-//         Modal
-//       </Button>
-//       <Modal
-//         title="Modal"
-//         open={open}
-//         onOk={hideModal}
-//         onCancel={hideModal}
-//         okText="确认"
-//         cancelText="取消"
-//       >
-//         <p>Bla bla ...</p>
-//         <p>Bla bla ...</p>
-//         <p>Bla bla ...</p>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// type Props = {
-//   onClose: () => void;
-// };
-
-// const LoginModal: React.FC<Props> = ({ onClose }) => {
-//   const [modal, contextHolder] = Modal.useModal();
-
-//   const confirm = () => {
-//     modal.confirm({
-//       title: "Confirm",
-//       icon: <ExclamationCircleOutlined />,
-//       content: "Bla bla ...",
-//       okText: "确认",
-//       cancelText: "取消",
-//     });
-//   };
-
-//   return (
-//     <>
-//       <Space>
-//         <LocalizedModal />
-//         <Button onClick={confirm}>Confirm</Button>
-//       </Space>
-//       {contextHolder}
-//     </>
-//   );
-// };
-
-// export default LoginModal;
-
 import React from "react";
 import { Modal } from "antd";
-
+import Interface from "./interface";
 type Props = {
   onClose: () => void;
   open: boolean;
@@ -75,14 +9,13 @@ type Props = {
 const LoginModal: React.FC<Props> = ({ onClose, open }) => {
   return (
     <Modal
-      title="Login"
       open={open}
       onCancel={onClose}
       footer={null}
       maskClosable={false}
+      centered={true}
     >
-      <p>Welcome to the login modal!</p>
-      <p>Please enter your credentials.</p>
+      <Interface />
     </Modal>
   );
 };
