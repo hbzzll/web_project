ca507a9dc36f3e242952191e3a56720c7a5dbaff
hbzzll <hbzz0823@163.com>
2025-04-13 12:46:07 +0200

    login and signup component

--- src/store/reducer.tsx
+++ src/store/reducer.tsx
@@ -1,23 +1,45 @@
-import newState from "./Numstatus";
+import { RadarChartOutlined } from "@ant-design/icons";
+import { createSlice } from "@reduxjs/toolkit";
+import { request } from "../utils/request";
 
-const defaultState = {
-  ...newState,
+const usetStore = createSlice({
+  name: "user",
+  initialState: {
+    token: localStorage.getItem("token_key") || "",
+  },
+
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload;
+
+      //store in loacalstorage
+      localStorage.setItem("token_key", action.payload);
+    },
+  },
+});
+
+const { setToken } = usetStore.actions;
+
+const userReducer = usetStore.reducer;
+
+// Define the type for loginForm
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  msg: string;
+  token: string;
+}
+
+const fetchLogin = (loginForm: LoginForm) => {
+  return async (dispatch: (action: any) => void) => {
+    const res: LoginResponse = await request.post("/api/register", loginForm);
+    dispatch(setToken(res.token));
+  };
 };
 
-let reducer = (state = defaultState, action: { type: string; val: number }) => {
-  let newState = JSON.parse(JSON.stringify(state));
+export { fetchLogin, setToken };
 
-  switch (action.type) {
-    case "add1":
-      newState.actions.add1(newState, action);
-      break;
-    case "add2":
-      newState.actions.add2(newState, action);
-      break;
-    default:
-      break;
-  }
-  return newState;
-};
-
-export default reducer;
+export default userReducer;
